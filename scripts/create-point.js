0ab43eca9f07c689f9056a95f28216aa574d00f0
Wesley Anjos <wanjos.andrade@gmail.com>
2020-06-04 03:06:27 -0300

    Correção do resultado das cidades e implementação das ações para seleção dos itens de coleta

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -39,6 +39,10 @@ function getCities(event) {
   //URL Da API, atribuindo ID do Estado na URL
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
   
+  //Realizando a limpeza do conteúdo citySelect
+  citySelect.innerHTML = "<option value>Selecione a Cidade</option>";
+  //Desabilitando o campo cidade
+  citySelect.disabled = true;
 
   fetch(url)
     .then(res =>
@@ -46,7 +50,9 @@ function getCities(event) {
     )
     .then(cities => {
       for(const city of cities) {
-        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        //Pegando nome da cidade para realizar o salvamento
+        //Vai coletar o nome do Estado e a Cidade
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
       }
       //Habilitando select Cidades
       citySelect.disabled = false;
@@ -56,3 +62,66 @@ function getCities(event) {
 //Selecionando campo select Estado
 document.querySelector("select[name=uf]")
   .addEventListener("change", getCities)
+
+// Itens de coleta
+
+//Pegando todos li's
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+//Para cada item capturado será realizado uma ação
+for(const item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem)
+}
+
+//Pegando input hidden que irá armazenar os items de coleta que foi selecionado
+const collectedItems = document.querySelector("input[name=items]")
+
+//Itens selecionados
+let selectedItems = [];
+
+function handleSelectedItem(event) {
+  const itemLi = event.target
+
+  //Adicionar ou remover classe do itemLi
+  //Caso não tenha classe ele adiciona
+  //Caso tenha classe selected ele retira  
+  itemLi.classList.toggle("selected")
+
+
+  //Coletando data-id
+  const itemId = itemLi.dataset.id;
+
+
+  //Verificar se existe items selecionado, se sim
+  //Pegar os itens selecionados
+
+  const alreadySelected = selectedItems.findIndex(item => {
+    //Para cada item que for coletado será realizado procedimento
+    return item == itemId //Retorna true ou false
+  })
+
+  //Se já estiver selecionado, tirar da seleção
+  //-1 Não está selecionado
+
+  //Se alreadySelected for maior ou igual a 0 então já possui index selecionado
+  if(alreadySelected >= 0) {
+    //Tirando da seleção que está no array
+    const filteredItems = selectedItems.filter(item => {
+      //Caso retorne false retira ele do novo array, sendo filtrado
+      //Caso retorne true adiciona ele no novo array (itemIsDifferente)
+      const itemIsDifferente = item != itemId
+      return itemIsDifferente
+    })
+
+    //Recebendo os itens filtrados
+    selectedItems = filteredItems
+    
+  } else {
+    //Se não estiver selecionado, adicionar à seleção
+    selectedItems.push(itemId)
+  }
+ 
+  //Atualizando o campo escondido com os itens selecionados
+  collectedItems.value = selectedItems
+
+}
